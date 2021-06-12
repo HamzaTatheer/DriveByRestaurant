f931718c3babb1e13cb0614d6a87e4052fd24e39
f-lat <l181127@lhr.nu.edu.pk>
2021-06-12 16:25:32 +0500

    deleted my folder picking affaqs

--- backend/routes/cashier.js
+++
@@ -1,2 +0,0 @@
-//change food status
-//view all orders
--- backend/startup/publicFolders.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-//const path = require('path');
-
-module.exports = function(app){
-    //var public = path.join(__dirname, 'public'); may be useful later
-
-    app.use('/public/uploads/profile_pictures',express.static('public/uploads/profile_pictures'));
-    app.use('/public/uploads/food_items',express.static('public/uploads/food_items'));
-}
