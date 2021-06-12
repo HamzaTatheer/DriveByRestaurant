69dd3617793775268bafa5241de0e57eecbce212
Affaq Ahmed <65063390+Affaq-Ahmed@users.noreply.github.com>
2021-06-12 15:16:16 +0500

    Delete backend directory
    Refactor

--- backend/sockets/customerServiceProvider.js
+++
@@ -1,4 +0,0 @@
-//cashier join cashier support room
-//cashier sends a message by @03040485173 hello
-//customer support message is sent to both cashier support room and room with phone "03040485173"
-//food status change
